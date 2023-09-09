fd3b5a4283551700fb92c46e063c9c762ecf891f
seokjinwoo <34178703+seokjinwoo@users.noreply.github.com>
2023-09-09 19:25:58 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -100,7 +100,3 @@ st.pyplot(fig)
 
 
 
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
