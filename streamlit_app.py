049773b447c2e9e3e67f0c389cc6d5b9a18d8613
seokjinwoo <34178703+seokjinwoo@users.noreply.github.com>
2023-09-14 20:49:32 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,19 +7,24 @@ import numpy as np
 st.markdown("""
 <style>
     .reportview-container {
-        background-color: #f4f6f6;
+        background-color: #f0f2f6;
     }
     .big-font {
         font-size:50px !important;
+        color: #333;
     }
     .small-font {
         font-size:16px !important;
+        color: #666;
+    }
+    h1 {
+        color: #333;
     }
 </style>
 """, unsafe_allow_html=True)
 
 st.markdown('<div class="big-font">국세 진도율에 대한 산포도</div>', unsafe_allow_html=True)
-st.markdown('명지대 경제학과', unsafe_allow_html=True)
+st.markdown('중요한경제문제연구소', unsafe_allow_html=True)
 
 # Load the data
 @st.cache
@@ -46,11 +51,11 @@ filtered_data = df[df['cat'] == selected_cat]
 # Scatter plot
 st.write(f"{selected_cat} Scattergram")
 fig, ax = plt.subplots()
-ax.scatter(np.array(filtered_data['month']), np.array(filtered_data['pro']), alpha=0.3, label='Scatterplot')
+ax.scatter(np.array(filtered_data['month']), np.array(filtered_data['pro']), alpha=0.5, color='#0077B6', label='Scatterplot')
 
 # Plotting 2023 data with a noticeable color line
 data_2023 = filtered_data[filtered_data['year'] == 2023]
-ax.plot(np.array(data_2023['month']), np.array(data_2023['pro']), color='magenta', label='2023')
+ax.plot(np.array(data_2023['month']), np.array(data_2023['pro']), color='#FF6347', label='2023')
 
 # Plotting average 'pro' values for years before 2023 with dashed line
 avg_pro_before_2023 = filtered_data[filtered_data['year'] <= 2022].groupby('month')['pro'].mean()
@@ -59,10 +64,11 @@ ax.plot(np.array(avg_pro_before_2023.index), np.array(avg_pro_before_2023.values
 # Setting x-axis labels with abbreviated month names
 months_abbrev = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
 ax.set_xticks(range(1, 13))
-ax.set_xticklabels(months_abbrev, rotation=45)
+ax.set_xticklabels(months_abbrev, rotation=45, color='#555')
+ax.set_yticklabels(ax.get_yticks(), color='#555')
 
 ax.set_xlabel('')
-ax.set_ylabel('Revenue progress rate (%)')
+ax.set_ylabel('Revenue progress rate (%)', color='#555')
 ax.legend()
 st.pyplot(fig)
 
